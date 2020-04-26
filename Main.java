f6e3a1b5860172dd3166383529bcfedb314ab60a
rushilp2311 <rushilp2311@gmail.com>
2020-04-25 22:05:13 -0400

    BST Contruction

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
+        BinarySearchTree.BST b = new BinarySearchTree.BST(20);
+        System.out.println(b.insert(10));
+        System.out.println(b.contains(10));
+        System.out.println(b.contains(30));
 
     }
 }
