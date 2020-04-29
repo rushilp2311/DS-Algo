46f11a0238e8505e0f93f6a38f064e754ce2f5b3
rushilp2311 <rushilp2311@gmail.com>
2020-04-29 14:28:54 -0400

    New Questions

--- TwoSum.java
+++ TwoSum.java
@@ -1,19 +1,18 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 public class TwoSum {
-    public static int[] twoNumberSum(int[] array,int targetSum){
+    public static int[] twoNumberSum(int[] array, int targetSum) {
         Arrays.sort(array);
         int left = 0;
         int right = array.length - 1;
-        while(left < right){
+        while (left < right) {
             int currentSum = array[left] + array[right];
-            if(currentSum == targetSum) {
-                return new int[]{array[left], array[right]};
-            }else if(currentSum < targetSum){
-                left ++;
-            }
-            else{
+            if (currentSum == targetSum) {
+                return new int[] { array[left], array[right] };
+            } else if (currentSum < targetSum) {
+                left++;
+            } else {
                 right--;
             }
         }
--- singleNumber.java
+++ singleNumber.java
@@ -1,16 +1,15 @@
-import java.util.HashMap;
 
 public class singleNumber {
-    public int Number(int[] nums){
-//        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-//        for(int i : nums){
-//            map.put(i,map.get(i) == null ? 1 : map.get(i)+1);
-//        }
-//
-//        for(int i : map.keySet()){
-//            if (map.get(i) == 1) return i;
-//        }
-//        return 0;
+    public int Number(int[] nums) {
+        // HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        // for(int i : nums){
+        // map.put(i,map.get(i) == null ? 1 : map.get(i)+1);
+        // }
+        //
+        // for(int i : map.keySet()){
+        // if (map.get(i) == 1) return i;
+        // }
+        // return 0;
 
         // Better approach is bit manipulation using XOR
         int a = 0;
@@ -23,7 +22,7 @@ public class singleNumber {
 
     public static void main(String[] args) {
         singleNumber s = new singleNumber();
-        int[] n = {4,2,1,2};
+        int[] n = { 4, 2, 1, 2 };
         System.out.println(s.Number(n));
     }
 }
