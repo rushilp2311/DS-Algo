8245b0ba7c20b8bca52bb0395d538aa0ee820f6f
rushilp2311 <rushilp2311@gmail.com>
2020-04-29 14:25:17 -0400

    New Question

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        int [] nums = {0,1,0,3,12};
+        MoveZeros.moveZeros(nums);
     }
 }
--- MoveZeros.java
+++ MoveZeros.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class MoveZeros {
+    public static void moveZeros(int[] nums) {
+//        int counter = 0;
+//
+//        for(int i = nums.length-1; i >= 0;i-- ){
+//            if(nums[i] == 0){
+//                counter = i;
+//                while(counter < nums.length-1){
+//                    int temp = 0;
+//                    temp = nums[counter];
+//                    nums[counter] = nums[counter+1];
+//                    nums[counter+1] = temp;
+//                    counter++;
+//                }
+//            }
+//        }
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] !=0){
+                nums[count] = nums[i];
+                count++;
+            }
+
+        }
+        while(count < nums.length){
+            nums[count++] = 0;
+        }
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
 }
