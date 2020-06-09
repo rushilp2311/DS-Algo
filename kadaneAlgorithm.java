181ff24489040b49a0f5c3d7947d1d380822757b
rushilp2311 <rushilp2311@gmail.com>
2020-06-08 22:29:20 -0400

    Kadane Algorithm Added

---
+++ kadaneAlgorithm.java
@@ -0,0 +1,14 @@
+public class kadaneAlgorithm {
+    public static int kadaneAlgorithm(int[] array){
+    int maxEndingHere = array[0];
+    int maxSoFar = array[0];
+    for(int i = 1; i< array.length;i++){
+        int num = array[i];
+        maxEndingHere = Math.max(num,maxEndingHere + num);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+    }
+
+
+}
