1e8893d322fc54359e3d926779d7c18481b7bf3b
rushilp2311 <rushilp2311@gmail.com>
2020-07-02 08:47:45 -0400

    AdventOfCode Day 2

---
+++ SymmetricTree.java
@@ -0,0 +1,22 @@
+public class SymmetricTree {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val) {this.val = val;}
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static boolean isSymmetric(TreeNode root){
+        return isMirror(root,root);
+    }
+    public static boolean isMirror(TreeNode t1, TreeNode t2){
+        if(t1 == null && t2 == null) return true;
+        if(t1 == null || t2 == null) return false;
+        return (t1.val == t2.val) && isMirror(t1.right,t2.left) && isMirror(t1.left,t2.right);
+    }
+}
